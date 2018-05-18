eff2196579940458aa84643df2fdde817d49adb7
happyandhappy <greyfrapps@gmail.com>
2018-05-18 11:14:27 +0800

    f

---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Welcome to my profile</title>
+<link type="text/css" rel="stylesheet" href="css/brown.css" />
+<link type="text/css" rel="stylesheet" href="css/print.css" media="print"/>
+<!--[if IE 7]>
+<link href="css/ie7.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+<!--[if IE 6]>
+<link href="css/ie6.css" rel="stylesheet" type="text/css" />
+<![endif]-->
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/jquery.tipsy.js"></script>
+<script type="text/javascript" src="js/cufon.yui.js"></script>
+<script type="text/javascript" src="js/scrollTo.js"></script>
+<script type="text/javascript" src="js/myriad.js"></script>
+<script type="text/javascript" src="js/jquery.colorbox.js"></script>
+<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript">
+		Cufon.replace('h1,h2');
+</script>
+</head>
+<body>
+<!-- Begin Wrapper -->
+<div id="wrapper">
+  <div class="wrapper-top"></div>
+  <div class="wrapper-mid">
+    <!-- Begin Paper -->
+    <div id="paper">
+      <div class="paper-top"></div>
+      <div id="paper-mid">
+        <div class="entry">
+          <!-- Begin Image -->
+          <img class="portrait" src="images/photo.jpg" alt="John Smith" />
+          <!-- End Image -->
+          <!-- Begin Personal Information -->
+          <div class="self">
+            <h1 class="name">Widad Saghir<br/>
+              <span style="margin-top: 3px!important;">Web/Mobile developer + Computer vision Expert</span></h1>
+            <ul>
+              <li class="ad">19 lot El Fath alley Bouganvilliers, Ain sebaa,  Casablanca , MOROCCO</li>
+              <li class="mail">widadsaghir1993@gmail.com</li>
+              <!-- <li class="tel">+11 444 555 22 33</li> -->
+              <!-- <li class="web">www.businessweb.com</li> -->
+            </ul>
+          </div>
+          <!-- End Personal Information -->
+          <!-- Begin Social -->
+          <div class="social">
+            <ul>
+              <li><a class='north' href="#" title="Download .pdf"><img src="images/icn-save.jpg" alt="Download the pdf version" /></a></li>
+              <li><a class='north' href="javascript:window.print()" title="Print"><img src="images/icn-print.jpg" alt="" /></a></li>
+              <li><a class='north' id="contact" href="contact/index.html" title="Contact Me"><img src="images/icn-contact.jpg" alt="" /></a></li>
+              <!-- <li><a class='north' href="#" title="Follow me on Twitter"><img src="images/icn-twitter.jpg" alt="" /></a></li>
+              <li><a class='north' href="#" title="My Facebook Profile"><img src="images/icn-facebook.jpg" alt="" /></a></li> -->
+            </ul>
+          </div>
+          <!-- End Social -->
+        </div>
+        <!-- Begin 1st Row -->
+        <div class="entry">
+          <h2>OBJECTIVE</h2>
+          <p>I have 7 years experience in Computer vision and machine learning.
+Computer vision like OCR, Face detection and recognition, Object tracking and image processing is advanced science in real world. this technique is widely used in our life and office.
+My enough experience and deep knowledge will help for our happy life and our pretty future.</p>
+        </div>
+        <!-- End 1st Row -->
+        <!-- Begin 2nd Row -->
+        <div class="entry">
+          <h2>EDUCATION</h2>
+          <div class="content">
+            <h3>Sep 2010 - Feb 2013</h3>
+            <p>Université Sidi Mohammed Ben Abdellah-Fès, Morocco <br />
+              <em>Master in Computer Science</em></p>
+          </div>
+        </div>
+        <!-- End 2nd Row -->
+        <!-- Begin 3rd Row -->
+        <div class="entry">
+          <h2>EXPERIENCE</h2>
+          <div class="content">
+            <h3>May 2014 - Feb 2017</h3>
+            <p>Terra Networks<br />
+              <em>Senior Web Developer</em></p>
+            <ul class="info">
+              <li>Have always concentrated to Modern Style</li>
+              <li>Feeling Smart and Modern <a href="https://howtube.com/">Style</a> in Website </li>
+            </ul>
+          </div>
+        </div>
+        <!-- End 3rd Row -->
+        <!-- Begin 4th Row -->
+        <div class="entry">
+          <h2>SKILLS</h2>
+          <div class="content">
+            <h3>Programming languages</h3>
+            <ul class="skills">
+              <li>Core PHP</li>
+              <li>Laravel</li>
+              <li>Codeignitor</li>
+              <li>Cake PHP</li>
+              <li>Node.js</li>
+              <li>Django</li>
+              <li>Angular.js</li>
+              <li>Angular 2,4,5</li>
+              <li>React.js</li>
+              <li>Bootstrap</li>
+              <li>JavaScript</li>
+              <li>CSS/XHTML</li>
+              <li>Ruby on Rails</li>
+              <li>ActionScript</li>
+              <li>Java</li>
+              <li>C++</li>
+              <li>C#</li>
+              <li>Python</li>
+            </ul>
+          </div>
+        </div>
+        <!-- End 4th Row -->
+         <!-- Begin 5th Row -->
+        <div class="entry">
+        <h2>WORKS</h2>
+        	<ul class="works">
+        		<li><a href="images/1.png" rel="gallery" title="https://www.howtube.com/"><img src="images/1.png" alt="" /></a></li>
+        		<li><a href="images/2.png" rel="gallery" title="Dating site"><img src="images/2.png" alt="" /></a></li>
+        		<li><a href="images/3.png" rel="gallery" title="photographrgv.com"><img src="images/3.png" alt="" /></a></li>
+        		<li><a href="images/4.png" rel="gallery" title="www.invictawatch.com"><img src="images/4.png" alt="" /></a></li>
+        		<li><a href="images/5.png" rel="gallery" title="http://hanifinbuilders.com/"><img src="images/5.png" alt="" /></a></li>
+        		<li><a href="images/6.jpg" rel="gallery" title="https://sprizzy.com"><img src="images/6.png" alt="" /></a></li>
+        		<li><a href="images/7.jpg" rel="gallery" title="www.cryptosis.org"><img src="images/7.png" alt="" /></a></li>
+        		<li><a href="images/8.jpg" rel="gallery" title="Image processing"><img src="images/8.png" alt="" /></a></li>
+        	</ul>
+        </div>
+        <!-- Begin 5th Row -->
+      </div>
+      <div class="clear"></div>
+      <div class="paper-bottom"></div>
+    </div>
+    <!-- End Paper -->
+  </div>
+  <div class="wrapper-bottom"></div>
+</div>
+<div id="message"><a href="#top" id="top-link">Go to Top</a></div>
+<!-- End Wrapper -->
+</body>
+</html>
